2ac32d593351343314db7abdfe6cf40c43c71987
James Ellis <j@athleticsnyc.com>
2014-09-02 12:33:49 -0400

    Changed style-related methods, allowing one to get an array of styles (style_list) in addition to the existing style method that returns complete <link> tags.

--- extensions/helper/Assets.php
+++ extensions/helper/Assets.php
@@ -8,22 +8,48 @@ class Assets extends \lithium\template\Helper {
 
 	/**
 	 * Takes a manifest name and:
-	 * 1. Compiles less files to css.
-	 * 2. Merges all files in the manifest (if $config['mergeAssets'] is true).
-	 * 3. Splits files by IE selector count limint (if $config['blessCss'] is true).
-	 * 4. Returns one or more style link tags.
+	 * 1. Generates the corresponding CSS files based on rules of $this->gen_css_files
+	 * 2. Returns one or more style link tags.
 	 *
 	 * @param  string $manifest name of manifest
 	 * @return string stylesheet `<link>` tag(s)
 	 */
 	public function style($manifest) {
-		$manifest = Manifest::load('css', $manifest);
-		$files = $manifest->build();
+		$files = $this->gen_css_files($manifest);
 		$tags = array();
 		foreach ($files as $file) {
 			$tags[] = $this->_context->helper('html')->style($file);
 		}
 		return implode("\n", $tags);
+	}
+
+	/**
+	 * Takes a manifest name and:
+	 * 1. Generates the corresponding CSS files based on rules of $this->gen_css_files
+	 * 2. Returns array of CSS files.
+	 *
+	 * @param  string $manifest name of manifest
+	 * @return array stylesheet files
+	 */
+	public function style_list($manifest) {
+		$files = $this->gen_css_files($manifest);
+		return $files;
+	}
+
+	/**
+	 * Takes a manifest name and:
+	 * 1. Compiles less files to css.
+	 * 2. Merges all files in the manifest (if $config['mergeAssets'] is true).
+	 * 3. Splits files by IE selector count limint (if $config['blessCss'] is true).
+	 * 4. Returns array of CSS files
+	 *
+	 * @param  string $manifest name of manifest
+	 * @return array stylesheet files
+	 */
+	private function gen_css_files($manifest) {
+		$manifest = Manifest::load('css', $manifest);
+		$files = $manifest->build();
+		return $files;
 	}
 
 	/**
