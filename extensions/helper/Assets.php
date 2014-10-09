c021efa6198b201ddada510c59b013265a4af0c7
Brandon Westcott <brandon.westcott@vitals.com>
2014-10-09 10:21:31 -0400

    Cleanup Asset class and expose script_list method

--- extensions/helper/Assets.php
+++ extensions/helper/Assets.php
@@ -8,48 +8,27 @@ class Assets extends \lithium\template\Helper {
 
 	/**
 	 * Takes a manifest name and:
-	 * 1. Generates the corresponding CSS files based on rules of $this->gen_css_files
+	 * 1. Generates the corresponding CSS files based on rules of $this->_build
 	 * 2. Returns one or more style link tags.
 	 *
 	 * @param  string $manifest name of manifest
 	 * @return string stylesheet `<link>` tag(s)
 	 */
 	public function style($manifest) {
-		$files = $this->gen_css_files($manifest);
-		$tags = array();
-		foreach ($files as $file) {
-			$tags[] = $this->_context->helper('html')->style($file);
-		}
-		return implode("\n", $tags);
+		$files = $this->style_list($manifest);
+		return $this->_html('style', $files);
 	}
 
 	/**
 	 * Takes a manifest name and:
-	 * 1. Generates the corresponding CSS files based on rules of $this->gen_css_files
+	 * 1. Generates the corresponding CSS files based on rules of $this->_build
 	 * 2. Returns array of CSS files.
 	 *
 	 * @param  string $manifest name of manifest
 	 * @return array stylesheet files
 	 */
 	public function style_list($manifest) {
-		$files = $this->gen_css_files($manifest);
-		return $files;
-	}
-
-	/**
-	 * Takes a manifest name and:
-	 * 1. Compiles less files to css.
-	 * 2. Merges all files in the manifest (if $config['mergeAssets'] is true).
-	 * 3. Splits files by IE selector count limint (if $config['blessCss'] is true).
-	 * 4. Returns array of CSS files
-	 *
-	 * @param  string $manifest name of manifest
-	 * @return array stylesheet files
-	 */
-	private function gen_css_files($manifest) {
-		$manifest = Manifest::load('css', $manifest);
-		$files = $manifest->build();
-		return $files;
+		return $this->_build('css', $manifest);
 	}
 
 	/**
@@ -62,11 +41,48 @@ class Assets extends \lithium\template\Helper {
 	 * @return string javascript script tag(s)
 	 */
 	public function script($manifest) {
-		$manifest = Manifest::load('js', $manifest);
-		$files = $manifest->build();
+		$files = $this->script_list($manifest);
+		return $this->_html('script', $files);
+	}
+
+	/**
+	 * Takes a manifest name and:
+	 * 1. Generates the corresponding JS files based on rules of $this->_build
+	 * 2. Returns array of JS files.
+	 *
+	 * @param  string $manifest name of manifest
+	 * @return array js files
+	 */
+	public function script_list($manifest) {
+		return $this->_build('js', $manifest);
+	}
+
+	/**
+	 * Takes a manifest name and:
+	 * 1. Compiles less and coffee files
+	 * 2. Merges all files in the manifest (if $config['mergeAssets'] is true).
+	 * 3. Returns array of files
+	 *
+	 * @param  string $type of asset
+	 * @param  string $manifest name of manifest
+	 * @return array of files
+	 */
+	protected function _build($type, $manifest) {
+		$manifest = Manifest::load($type, $manifest);
+		return $manifest->build();
+	}
+
+	/**
+	 * Takes an html method and list of files
+	 *
+	 * @param  string $method to be called on html helper
+	 * @param  array $files  array of files
+	 * @return string with baked html
+	 */
+	protected function _html($method, $files) {
 		$tags = array();
 		foreach ($files as $file) {
-			$tags[] = $this->_context->helper('html')->script($file);
+			$tags[] = $this->_context->helper('html')->$method($file);
 		}
 		return implode("\n", $tags);
 	}
